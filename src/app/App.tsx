7938161b69770eb9c5db9b92968e68161f35423c
Sayan Chakraborty <sayanchakraborty@Sayans-MacBook-Pro.local>
2026-07-21 00:52:57 +0530

    vercel speed insight update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { router } from "./routes";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <>
       <RouterProvider router={router} />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
